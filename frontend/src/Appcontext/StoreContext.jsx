a651024cb531c0a6b0165759a4ae2ccaea8f8875
MohdMuttavaquil <mmuttavaquil@gmail.com>
2026-02-24 12:50:00 +0530

    Add new url

--- frontend/src/Appcontext/StoreContext.jsx
+++ frontend/src/Appcontext/StoreContext.jsx
@@ -5,7 +5,8 @@ export const StoreContext = createContext();
 export function StoreProvider({ children }) {
 
   //const url = "http://localhost:3000"
-  const url = "https://college-website-beckend.onrender.com"
+  //const url = "https://college-website-beckend.onrender.com"
+  const url = 'http://13.201.30.142:3000'
   const [profile, setProfile] = useState(false);
 
   return (
